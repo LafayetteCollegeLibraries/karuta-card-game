c43ccd944bd2a066493fb769fa68543a7d314bd5
adam malantonio <malantoa@lafayette.edu>
2017-05-24 10:45:05 -0400

    preload strategy?

--- src/index.js
+++ src/index.js
@@ -9,3 +9,22 @@ const App = props => {
 }
 
 render(<App/>, document.querySelector('#karuta-game-app'))
+
+// preload to avoid delay?
+
+// const target = document.createElement('div')
+// target.style.display = 'none'
+
+// config.cards.map(item => {
+//   const { recitation, grab } = item
+//   const imgs = [recitation, grab]
+
+//   imgs.forEach(i => {
+//     const img = document.createElement('img')
+//     img.style.display = 'none'
+//     img.src = i
+
+//     target.appendChild(img)
+//     target.removeChild(img)
+//   })
+// })
